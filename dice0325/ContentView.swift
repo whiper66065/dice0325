ae2ea1b9d57649c28ce59ecedf39503ac3b3fdfc
yujenpan <whiper66065@gmail.com>
2020-03-26 19:40:23 +0800

    Initial Commit

---
+++ dice0325/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dice0325
+//
+//  Created by 潘昱任 on 2020/3/26.
+//  Copyright © 2020 ntouyujen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
