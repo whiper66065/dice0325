9f351555d5952c9ea14ae0d075b6e731a312d5d2
yujenpan <whiper66065@gmail.com>
2020-04-02 18:24:16 +0800

    diceHWfinish

--- dice0325/ContentView.swift
+++ dice0325/ContentView.swift
@@ -9,8 +9,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAlert = false
+    @State private var Num : Int = 0
+    @State private var color:[Color] = [.red,.green,.gray,.pink,.orange,.purple,.yellow]
+    @State private var player = ["就是你自己！","Lebron James","Kevin Durant","James Harden","Chris Paul","Anthony Davis","Lou Williams"]
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Image("nbalogoblue")
+            .resizable()
+            .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity)
+            VStack(alignment: .center){
+                Text("看看今天的幸運數字及球員？")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .cornerRadius(10.0)
+                    .offset(x:0, y:-100)
+                ZStack{
+                    Capsule()
+                        .frame(width: 300, height: 200)
+                    .foregroundColor(color[Num])
+                    Text("\(Num)")
+                        .font(.system(size: 150))
+                        .foregroundColor(Color.black)
+                }
+                Button(action:{
+                    let randNum = 0...6
+                    self.Num = randNum.randomElement()!
+                    self.showAlert = true
+                }){
+                    
+                    Text("請按我")
+                        .font(.system(size: 50))
+                        .foregroundColor(.black)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(50)
+                        .shadow(radius: 10)
+                    
+                }
+                .offset(x:0, y:200)
+            
+                .alert(isPresented: $showAlert) { () -> Alert in
+                   
+                    return Alert(title: Text("您今天的幸運球員是："), message:Text(player[Num]), dismissButton: .cancel())
+                    
+                }
+                
+            }
+        }
+     
+        
     }
 }
 
